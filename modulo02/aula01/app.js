a1d4770804402c1e208a4faef67bc0cdbf03cf50
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-04-10 16:12:18 -0300

    aula01 modulo02

---
+++ modulo02/aula01/app.js
@@ -0,0 +1,16 @@
+// for loop
+/*for(let i = 0; i < 5; i++){
+    console.log(`Dentro do loop: ${i}`)
+}
+
+console.log('Loop concluído')*/
+
+const names = ['Linus', 'Ada', 'Bill']
+
+for(let i = 0; i < names.length; i++){
+    //console.log(i)
+    //console.log(names[i])
+    
+    const HTMLTemplate = `<p>${names[i]}</p>`
+    console.log(HTMLTemplate)
+}
